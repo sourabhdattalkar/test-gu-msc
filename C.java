d6dfcc4eec97582bd903bf1e4fa3efbecbae2293
unknown <sourabhdattalkar@gmail.com>
2022-11-24 09:34:28 +0530

    c file modified

--- C.java
+++ C.java
@@ -2,7 +2,7 @@ class A{
 void msg(){System.out.println("Hello");}  
 }  
 class B{  
-void msg(){System.out.println("Welcome");}  
+void msg(){System.out.println("Welcome india");}  
 }  
 class C extends A{
  public static void main(String args[]){  
